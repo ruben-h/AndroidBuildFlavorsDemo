c7671de83bad855258473eb557d5f3fe29f59f23
Envyandroid <envyandroid@gmail.com>
2015-01-17 23:20:34 +0100

    Create 2 build flavours, with different strings depending on flavor. Log variables

--- app/src/main/java/com/envyandroid/buildflavors/MainActivity.java
+++ app/src/main/java/com/envyandroid/buildflavors/MainActivity.java
@@ -2,6 +2,7 @@ package com.envyandroid.buildflavors;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String LOG_TAG = "BuildFlavorsExample";
+
+        //Flavor will be FREE or DONATE:
+        Log.d(LOG_TAG, "This is flavor: " + BuildConfig.FLAVOR);
+
+        //You can get hold of your variable from the buildConfigField in build.gradle:
+        //Will be true or false
+        Log.d(LOG_TAG, "This is version: " + BuildConfig.IS_DONATE);
     }
 
 
